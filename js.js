aee3bbd3b0e7ef736deebaa9b013bf02b64776c4
vishnugopy <vishnugoby0010@gmail.com>
2018-06-14 17:50:29 +0200

    Initial commit

---
+++ js.js
@@ -0,0 +1,7 @@
+function shown() {
+    document.getElementById("panel").style.display = "block";
+    }
+
+function noneshow() {
+    document.getElementById("panel").style.display = "none";
+    }
