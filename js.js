0672ae257dd6a8ba30ea799ac8922e48b0f67cd1
vishnugopy <vishnugoby0010@gmail.com>
2018-06-15 13:28:38 +0200

    today

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
-function shown() {
+/*function shown() {
     document.getElementById("panel").style.display = "block";
     }
 
 function noneshow() {
     document.getElementById("panel").style.display = "none";
-    }
+    }*/
